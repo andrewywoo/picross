384ec17b24afdf603fb25ca542a2add3773eb63d
Andrew Woo <andrew.woo@verizon.net>
2019-10-20 13:51:20 -0400

    finished indicators and styling with css grid. todo game winning as well as set grid when puzzle changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import "./App.css";
 import Board from "./container/Board/Board";
 
 function App() {
+  const puzzle = [
+    [true, false, false, false, false],
+    [true, true, true, true, false],
+    [false, false, true, true, true],
+    [false, false, true, true, true],
+    [true, true, true, true, false]
+  ];
   return (
     <div className="App">
       <h1>P I C R O S S</h1>
-      <Board></Board>
+      <Board puzzle={puzzle} />
     </div>
   );
 }
--- src/container/Board/Board.js
+++ src/container/Board/Board.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Box from "../../component/Box/Box";
 import "./Board.css";
-import Indicator from "../../component/Indicator/Indicator";
+import IndicatorRow from "../../component/IndicatorRow/IndicatorRow";
 
 const Board = props => {
   const [grid, setGrid] = useState([
@@ -12,54 +12,58 @@ const Board = props => {
     [false, false, false, false, false]
   ]);
 
-  const puzzle = [
-    [true, false, false, false, false],
-    [true, true, true, true, false],
-    [false, false, true, true, true],
-    [false, false, true, true, true],
-    [true, true, true, true, false]
-  ];
+  const [indicators, setIndicators] = useState({ top: [], side: [] });
 
-  function getTopIndicator() {
-    let result = [];
-    for (let i = 0; i < puzzle.length; i++) {
+  useEffect(() => {
+    //sets the indicators for the picross gameboard
+    getIndicators();
+  }, [props.puzzle]);
+
+  function getIndicators() {
+    let top = [];
+    let side = [];
+
+    //logic for top
+    for (let i = 0; i < props.puzzle.length; i++) {
       let column = [];
-      let count = 0;
-      puzzle.forEach(r => {
+      let topCount = 0;
+      props.puzzle.forEach(r => {
         if (r[i]) {
-          count++;
-        } else if (count !== 0 && r[i] === false) {
-          column.push(count);
-          count = 0;
+          topCount++;
+        } else if (topCount) {
+          column.push(topCount);
+          topCount = 0;
         }
       });
       //only push count if it isn't 0. this accounts for the last row of the column being true;
-      if (count) {
-        column.push(count);
+      if (topCount) {
+        column.push(topCount);
       }
-      //after counting indicator numbers. push to result and result column.
-      result.push(column);
+      //after counting indicator numbers. push to top and result topColumn.
+      top.push(column);
       column = [];
     }
-    //should iterate through columns of 2d-array and return an array of arrays for top indicator
-    return result;
-  }
 
-  function getSideIndicator(index) {
-    let count = 0;
-    let result = [];
-    puzzle[index].forEach(r => {
-      if (r) {
-        count++;
-      } else if (count !== 0 && r === false) {
-        result.push(count);
-        count = 0;
+    let sideCount = 0;
+    let row = [];
+    for (let i = 0; i < props.puzzle.length; i++) {
+      props.puzzle[i].forEach(r => {
+        if (r) {
+          sideCount++;
+        } else if (sideCount) {
+          row.push(sideCount);
+          sideCount = 0;
+        }
+      });
+      if (sideCount) {
+        row.push(sideCount);
+        sideCount = 0;
       }
-    });
-    if (count) {
-      result.push(count);
+      side.push(row);
+      row = [];
     }
-    return result;
+
+    setIndicators({ top: top, side: side });
   }
 
   //sets new state when a box is clicked.
@@ -76,34 +80,35 @@ const Board = props => {
 
   return (
     <div className="game-board">
-      <div className="row_indicator">
-        {getTopIndicator().map((numbers, index) => {
+      <div className="topInd">
+        <IndicatorRow
+          indicators={indicators.top}
+          orientation="horizontal"
+        ></IndicatorRow>
+      </div>
+      <div className="sideInd">
+        <IndicatorRow
+          indicators={indicators.side}
+          orientation="vertical"
+        ></IndicatorRow>
+      </div>
+      <div className="board">
+        {grid.map((row, rowIndex) => {
           return (
-            <div key={index} className="row_indicator_group">
-              <Indicator alignment="vertical" numbers={numbers}></Indicator>
+            <div key={rowIndex} className="row">
+              {row.map((checkVal, boxIndex) => {
+                return (
+                  <Box
+                    key={"" + rowIndex + boxIndex}
+                    checked={checkVal}
+                    clicked={() => handleBoxChange([rowIndex, boxIndex])}
+                  ></Box>
+                );
+              })}
             </div>
           );
         })}
       </div>
-      {grid.map((row, rowIndex) => {
-        return (
-          <div key={rowIndex} className="row">
-            <Indicator
-              alignment="horizontal"
-              numbers={getSideIndicator(rowIndex)}
-            ></Indicator>
-            {row.map((checkVal, boxIndex) => {
-              return (
-                <Box
-                  key={"" + rowIndex + boxIndex}
-                  checked={checkVal}
-                  clicked={() => handleBoxChange([rowIndex, boxIndex])}
-                ></Box>
-              );
-            })}
-          </div>
-        );
-      })}
     </div>
   );
 };
