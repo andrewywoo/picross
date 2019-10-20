d38471f222dd5c91669dc8d935f360855cb56812
Andrew Woo <andrew.woo@verizon.net>
2019-10-20 15:12:33 -0400

    Updates

---
+++ precache-manifest.d74f29539b174c2adb0df5b308357c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d207d45d100513b5164f256b1bcc330",
+    "url": "/picross/index.html"
+  },
+  {
+    "revision": "03fcf2e3fbbe84a41afc",
+    "url": "/picross/static/css/main.96339b5a.chunk.css"
+  },
+  {
+    "revision": "5996b152e29405b72a79",
+    "url": "/picross/static/js/2.b0fbf703.chunk.js"
+  },
+  {
+    "revision": "03fcf2e3fbbe84a41afc",
+    "url": "/picross/static/js/main.cb925bb2.chunk.js"
+  },
+  {
+    "revision": "a5fd2e8b9e3eeeaa68f2",
+    "url": "/picross/static/js/runtime-main.94d080f1.js"
+  }
+]);
